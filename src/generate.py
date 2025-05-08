025dd6793e384e98f91d42403e0f49aa351d3588
Tim-Mer <timmymerriman@gmail.com>
2025-05-08 16:27:00 -0700

    It works!!!!!!!!!!!

--- src/generate.py
+++ src/generate.py
@@ -1,5 +1,11 @@
 import os
 import shutil
+from textnode import *
+from htmlnode import *
+from funcs import *
+from blocks import *
+from md_to_html import *#
+from generate import *
 
 def clean_public(dir="./public"):
     logging(f"Removing {dir}")
@@ -22,3 +28,38 @@ def copy_static(input_dir="./static/", output_dir="./public/"):
 def logging(msg):
     print(f"LOG: {msg}")
     pass
+
+def extract_title(md):
+    if md == "":
+        raise Exception("Empty .md file")
+    title = ""
+    for line in md.split("\n"):
+        if line.strip().startswith('# '):
+            title = line.replace("# ", "")
+            break
+    if title == "":
+        raise Exception("No title found")
+    return title
+
+def generate_page(from_path, template_path, dest_path):
+    logging(f"Generating page from {from_path} to {dest_path}, using template {template_path}")
+    input_md = ""
+    with open(from_path, "r") as file:
+        input_md = file.read()
+    
+    template = ""
+    with open(template_path, "r") as file:
+        template = file.read()
+
+    input_html = markdown_to_html_node(input_md).to_html()
+    title = extract_title(input_md)
+    
+    output_html = template.replace("{{ Title }}", title).replace("{{ Content }}", input_html)
+    
+    with open(dest_path, "w") as file:
+        file.write(output_html)
+    
+    
+    
+    
+    
--- src/main.py
+++ src/main.py
@@ -8,29 +8,7 @@ from generate import *
 def main():
     clean_public()
     copy_static()
-    md = """
-This is a paragraph
-
-# just a h1 heading
-
-## h2 heading
-
-```
-code block
-```
-
-> this is a quote
-
-- unordered
-- list
-- blah blah blah
-
-1. ordered
-2. list
-
-end
-"""
-    #tmp = markdown_to_html_node(md)
-    #print(tmp.to_html())
+    
+    generate_page("./content/index.md", "template.html", "./public/index.html")
     
 main()

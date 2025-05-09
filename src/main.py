17d4c5ec8c129820f914e3b1925e6e614368a511
Tim-Mer <timmymerriman@gmail.com>
2025-05-08 20:12:40 -0700

    Update to work with /

--- src/main.py
+++ src/main.py
@@ -9,15 +9,17 @@ import os
 
 def main():
     basepath = "/"
+    public_path = "docs"
     if len(sys.argv) >= 2:
         basepath = sys.argv[1]
+        public_path = sys.argv[2]
     content_path = os.path.join("content")
-    public_path = os.path.join("docs")
+    public_path = os.path.join(public_path)
     static_path = os.path.join("static")
 
     clean_public(public_path)
     copy_static(static_path, public_path)
     
-    generate_pages_recursive(content_path, "template.html", public_path, basepath)
+    generate_pages_recursive(content_path, "src/template.html", public_path, basepath)
     
 main()
